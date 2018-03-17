8165f8296c1b15d1c5c42a2db35ef155a4e74faa
Monica Alvarez <mmalvar13@gmail.com>
2018-03-17 10:47:55 -0600

    refactored cat and types tables to come before actions

--- the-project/app/Category.php
+++ the-project/app/Category.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-	/**
+
+
+    protected $table = 'categories';
+
+    /**
 	 * The attributes that are mass assignable.
 	 *
 	 * @var array
