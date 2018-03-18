8e915b0b10dcea2b2460856b4bcb0b9edaf464c9
Samantha Andrews <samantharaeandrews@gmail.com>
2018-03-18 01:01:48 -0600

    added events view and edited some styles in actions view

--- the-project/resources/views/actions.blade.php
+++ the-project/resources/views/actions.blade.php
@@ -2,27 +2,28 @@
 
 @section('content')
     <section class="actions-purple-section">
-    <div class="container">
+        <div class="container">
             <h1 class="header-text white">Actions</h1>
-        <br>
-        <br>
-        @foreach($actions as $action)
-            <div class="row gray-bg actions">
-                <div class="col-sm-12 col-lg-4">
-                    <h5 class="actions-date">{{ $action->date }}</h5>
+            <br>
+            <br>
+            @foreach($actions as $action)
+                <div class="row gray-bg actions">
+                    <div class="col-sm-12 col-lg-4">
+                        <h5 class="actions-date">{{ $action->date }}</h5>
+                    </div>
+                    <div class="col-sm-12 col-lg-8">
+                        <h3>{{ $action->title }}</h3>
+                        <p>{{ $action->description }}</p>
+                        <a href="{{ $action->website }}">
+                            <div class="button-wrap">
+                                <button type="button" class="btn btn-green">Learn More + Take Action</button>
+                            </div>
+                        </a>
+                        <br>
+                        <p><strong>Call your representative: </strong>{{ $action->phone_number }}</p>
+                    </div>
                 </div>
-                <div class="col-sm-12 col-lg-8">
-                    <h3>{{ $action->title }}</h3>
-                    <p>{{ $action->description }}</p>
-                    <a href="{{ $action->website }}">
-                        <div class="button-wrap">
-                            <button type="button" class="btn btn-green">Take Action</button>
-                        </div>
-                    </a>
-                    <p>{{ $action->phone_number }}</p>
-                </div>
-            </div>
-        @endforeach
-    </div>
+            @endforeach
+        </div>
     </section>
 @stop
--- the-project/resources/views/events.blade.php
+++ the-project/resources/views/events.blade.php
@@ -1,12 +1,29 @@
 @extends('layout')
 
 @section('content')
-    <div>Hey we're in events</div>
-    @foreach($actions as $action)
-        <p>{{ $action->title }}</p>
-        <p>{{ $action->description }}</p>
-        <p>{{ $action->date }}</p>
-        <p>{{ $action->website }}</p>
-        <p>{{ $action->phone_number }}</p>
-    @endforeach
+    <section class="actions-purple-section">
+        <div class="container">
+            <h1 class="header-text white">Events</h1>
+            <br>
+            <br>
+            @foreach($actions as $action)
+                <div class="row gray-bg actions">
+                    <div class="col-sm-12 col-lg-4">
+                        <h5 class="actions-date">{{ $action->date }}</h5>
+                    </div>
+                    <div class="col-sm-12 col-lg-8">
+                        <h3>{{ $action->title }}</h3>
+                        <p>{{ $action->description }}</p>
+                        <a href="{{ $action->website }}">
+                            <div class="button-wrap">
+                                <button type="button" class="btn btn-green">Learn More + Take Action</button>
+                            </div>
+                        </a>
+                        <br>
+                        <p><strong>Call your representative: </strong>{{ $action->phone_number }}</p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </section>
 @stop
