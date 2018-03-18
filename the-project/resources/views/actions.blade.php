0b541ede0f4b097cda4d382fb68ff277673e6520
Samantha Andrews <samantharaeandrews@gmail.com>
2018-03-17 22:42:18 -0600

    styling actions view

--- the-project/resources/views/actions.blade.php
+++ the-project/resources/views/actions.blade.php
@@ -2,19 +2,31 @@
 
 @section('content')
     <div class="container">
-        <h1 class="blue-bg white">Actions</h1>
+        <div class="table">
+        <div class="header-text">
+            <div class="row">
+                <div class="col-md-12">
+            <h1>Actions</h1>
+                </div>
+            </div>
+        </div>
+        </div>
         @foreach($actions as $action)
             <div class="row gray-bg actions">
-                <div class="col-2">
-                    <h6>{{ $action->date }}</h6>
+                <div class="col-sm-12 col-lg-2">
+                    <h4>{{ $action->date }}</h4>
                 </div>
-                <div class="col-10">
+                <div class="col-sm-12 col-lg-10">
                     <h3>{{ $action->title }}</h3>
                     <p>{{ $action->description }}</p>
-                    <a>{{ $action->website }}</a>
+                    <a href="{{ $action->website }}">
+                        <div class="button-wrap">
+                            <button type="button" class="btn btn-info">Take Action</button>
+                        </div>
+                    </a>
                     <p>{{ $action->phone_number }}</p>
                 </div>
             </div>
+        @endforeach
     </div>
-    @endforeach
 @stop
