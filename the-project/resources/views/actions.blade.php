2e7c3ebfecd1a1cd9bb0880e947034dafb60c6f7
Samantha Andrews <samantharaeandrews@gmail.com>
2018-03-17 21:11:32 -0600

    added additional data types to the actions view

--- the-project/resources/views/actions.blade.php
+++ the-project/resources/views/actions.blade.php
@@ -3,5 +3,9 @@
 @section('content')
     @foreach($actions as $action)
         <p>{{ $action->title }}</p>
+        <p>{{ $action->description }}</p>
+        <p>{{ $action->date }}</p>
+        <p>{{ $action->website }}</p>
+        <p>{{ $action->phone_number }}</p>
     @endforeach
 @stop
