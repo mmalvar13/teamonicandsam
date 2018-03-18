6561913ea3922f17a4a057e76abafdf5066d0144
Monica Alvarez <mmalvar13@gmail.com>
2018-03-18 11:32:10 -0600

    changed header name

--- the-project/resources/views/partials/header.blade.php
+++ the-project/resources/views/partials/header.blade.php
@@ -5,7 +5,7 @@
                 <div class="row">
                     <div class="col-md-12 text-center">
                         <h3 class="light white">Take care of your community.</h3>
-                        <h1 class="white typed">Start fighting poverty today.</h1><span class="typed-cursor">|</span>
+                        <h1 class="white typed">How Can You Help?</h1><span class="typed-cursor">|</span>
                         <h3 class="light white">Choose an action below to get started</h3>
                     </div>
                 </div>
